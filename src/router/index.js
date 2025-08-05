543ec149368a6520cabba93ba3ecdc12baa5e6d0
GeunYeong <jgy091411@gmail.com>
2025-08-05 19:06:55 +0900

    로그인 화면 구성

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHashHistory } from "vue-router";
 import MemberCreate from "@/views/MemberCreate.vue"; // 회원가입 컴포넌트 경로
+import LoginPage from "@/views/LoginPage.vue"; // 회원가입 컴포넌트 경로
 
 const routes = [
   {
     path: "/member/create",
     name: "MemberCreate",
     component: MemberCreate,
+  },
+  {
+    path: "/login",
+    name: "LoginPage",
+    component: LoginPage,
   },
 ];
 
--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -3,11 +3,9 @@
     <v-row justify="center">
       <v-col cols="12" sm="4" md="6">
         <v-card>
-          <v-card-title class="text-h5 text-center">회원가입</v-card-title>
+          <v-card-title class="text-h5 text-center">로그인</v-card-title>
           <v-card-text>
-            <v-form @submit.prevent="memberCreate">
-              <v-text-field label="이름" v-model="name" required>
-              </v-text-field>
+            <v-form @submit.prevent="doLogin">
               <v-text-field label="email" v-model="email" type="email" required>
               </v-text-field>
               <v-text-field
@@ -17,7 +15,7 @@
                 required
               >
               </v-text-field>
-              <v-btn type="submit" color="primary" block>등록</v-btn>
+              <v-btn type="submit" color="primary" block>로그인</v-btn>
             </v-form>
           </v-card-text>
         </v-card>
@@ -25,31 +23,31 @@
     </v-row>
   </v-conatiner>
 </template>
-
 <script>
 import axios from "axios";
 
 export default {
   data() {
     return {
-      name: "", // 회원 이름
       email: "", // 회원 이메일
       password: "", // 회원 비밀번호
     };
   },
   methods: {
-    async memberCreate() {
-      const data = {
-        name: this.name,
+    async doLogin() {
+      const loginData = {
         email: this.email,
         password: this.password,
       };
 
-      await axios.post(
-        `${process.env.VUE_APP_API_BASE_URL}/member/create`,
-        data
+      const response = await axios.post(
+        `${process.env.VUE_APP_API_BASE_URL}/member/doLogin`,
+        loginData
       );
-      this.$router.push("/"); // 회원가입 후 홈으로 이동
+
+      const token = response.data.accessToken; // 로그인 성공 시 토큰 받기
+      localStorage.setItem("token", token); // 로컬 스토리지에 토큰 저장
+      window.location.href = "/"; // 로그인 후 홈으로 이동
     },
   },
 };
